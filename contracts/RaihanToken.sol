2b4087ffcea64044183ecbb4260c5f956b9960e6
Raihan <110191829+raihan-g-i-t@users.noreply.github.com>
2026-07-09 14:07:45 +0600

    Remove RaihanToken contract implementation

--- contracts/RaihanToken.sol
+++ contracts/RaihanToken.sol
@@ -1,15 +1,2 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-
-contract RaihanToken is ERC20, Ownable {
-    constructor(address initialOwner) ERC20("Raihan Token", "RAIHAN") Ownable(initialOwner) {
-        _mint(initialOwner, 1_000_000 * 10 ** decimals());
-    }
-
-    function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount);
-    }
-}
+//This code is private
